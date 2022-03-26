0d749cd3b49a4c45979eb2ac03171526657f27de
cch.chandra09 <candrasachio@gmail.com>
2022-03-26 08:33:54 +0700

    Create Model, Migration, Controller

--- database/migrations/2022_03_26_012216_create_report_users_table.php
+++ database/migrations/2022_03_26_012216_create_report_users_table.php
@@ -13,16 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('report_users', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('user_id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('role_id');
             $table->string('department');
             $table->string('division');
-            $table->rememberToken();
+            $table->text('report');
+            $table->string('img_path');
             $table->timestamps();
         });
     }
@@ -34,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('report_users');
     }
 };
